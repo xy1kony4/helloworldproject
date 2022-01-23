fc5868f2b851c3fa3f0a3a8f7391d0eac1ddef31
andrey <xy1kony4@yandex.ru>
2022-01-23 18:07:14 +0300

    set up main and loading screens

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button: UIButton!
+    @IBOutlet var helloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldLabel.isHidden = true
+        button.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAction() {
+        if helloWorldLabel.isHidden == true{
+            helloWorldLabel.isHidden = false
+            button.setTitle("Hide text", for: .normal)
+        }
+        else{
+            helloWorldLabel.isHidden = true
+            button.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
